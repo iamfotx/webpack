4d99803eb0c5b831bafd439112c98f40773ca639
Ahmad Raza <ahmedrazadev1@outlook.com>
2020-03-21 14:37:53 +0500

    add prettier

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import { top, bottom } from './footer';
 import makeButton from './button';
 import { makeColorStyle } from './button-styles';
 
-console.log(nav(), top, bottom, makeButton('my first button'), makeColorStyle('cyan'));
+console.log(
+  nav(),
+  top,
+  bottom,
+  makeButton('my first button'),
+  makeColorStyle('cyan'),
+);
