74f39b91c3f0b310c18dd9fec9518a20c2bd9251
Ahmad Raza <ahmedrazadev1@outlook.com>
2020-03-28 02:36:43 +0500

    split webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const webpack = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const webpackMerge = require('webpack-merge');
 
-module.exports = ({ mode }) => {
-    return {
-        mode,
-        output: {
-            filename: 'bundle.js',
+const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
+
+module.exports = ({ mode, presets } = { mode: 'production', presets: [] }) => {
+    return webpackMerge(
+        {
+            mode,
+            output: {
+                filename: 'bundle.js',
+            },
+            plugins: [new HTMLWebpackPlugin(), new webpack.ProgressPlugin()],
         },
-        plugins: [
-            new HTMLWebpackPlugin(),
-            new webpack.ProgressPlugin()
-        ]
-    };
+        modeConfig(mode),
+    );
 };
