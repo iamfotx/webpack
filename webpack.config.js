7f77b20e37dabe6d1047a9f57c225c9deafe78db
Ahmad Raza <ahmedrazadev1@outlook.com>
2020-03-28 01:50:00 +0500

    first plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = ({ mode }) => {
     return {
         mode,
         output: {
             filename: 'bundle.js',
         },
+        plugins: [
+            new HTMLWebpackPlugin(),
+            new webpack.ProgressPlugin()
+        ]
     };
 };
