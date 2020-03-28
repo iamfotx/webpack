22773c3816b9be77110896edcc5cad3584be4ee9
Ahmad Raza <ahmedrazadev1@outlook.com>
2020-03-28 23:54:35 +0500

    add mini-css-extract-plugin for production use

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import nav from './nav';
 import { top, bottom, footer } from './footer';
 import makeButton from './button';
 import { makeColorStyle, red } from './button-styles';
+import './footer.css';
+import './button.css';
 
 const button = makeButton('Yay! my first button!');
 button.style = makeColorStyle('cyan');
