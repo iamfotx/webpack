b59a341a4f3d4de0f01db6fb328b89a4b7f5a2e4
Ahmad Raza <ahmedrazadev1@outlook.com>
2020-03-30 02:22:20 +0500

    presets fix & code splitting

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import nav from './nav';
-import { top, bottom, footer } from './footer';
 import makeButton from './button';
 import { makeColorStyle, red } from './button-styles';
 import imageURL from './webpack-icon.jpg';
 import makeImage from './image';
-import './footer.css';
+const getFooter = () => import('./footer');
 import './button.css';
 
 const image = makeImage(imageURL);
 const button = makeButton('Yay! my first button!');
 button.style = makeColorStyle('cyan');
 document.body.appendChild(button);
-document.body.appendChild(footer);
 document.body.appendChild(image);
-
+button.addEventListener('click', function clickListener(e) {
+    // show the footer here
+    getFooter().then(({ footer }) => {
+        document.body.appendChild(footer);
+    });
+});
 console.log(nav());
